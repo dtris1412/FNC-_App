859af10f6353b2ca782e7fa24d8e125191f7558d
dtris <daohuutri04@gmail.com>
2025-02-15 14:42:46 +0700

    test2

--- app/src/main/java/com/example/fnc_app/HomeFragment.java
+++ app/src/main/java/com/example/fnc_app/HomeFragment.java
@@ -6,23 +6,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ViewFlipper;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
 
+    private ViewFlipper vf;
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_home, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        // Khởi tạo ViewFlipper trong Fragment
+        vf = view.findViewById(R.id.vf);
+        vf.setFlipInterval(4000);
+        vf.setAutoStart(true);
+        vf.startFlipping();
+
+        return view;
     }
-
-
-
 }
